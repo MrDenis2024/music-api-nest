a4daf33d69c83f9aacbf9422bbdd48796462ed5b
Khrunev Denis <deniskhrunev@gmail.com>
2024-10-09 15:58:54 +0600

    Создана схемы для сущностей

---
+++ src/schemas/album.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Artist } from './artist.schema';
+
+export type AlbumDocument = Album & Document;
+
+@Schema()
+export class Album {
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Artist' })
+  artist: Artist;
+  @Prop({ required: true })
+  title: string;
+  @Prop({ required: true })
+  year: number;
+  @Prop()
+  image: string;
+}
+
+export const AlbumSchema = SchemaFactory.createForClass(Album);
